7da7d3892256a997820a403264863f67197058c6
Marcel <52205196+marcel-dev-acc@users.noreply.github.com>
2020-06-22 10:51:52 +0100

    Update urls.py
    🖐

--- app/djprog/urls.py
+++ app/djprog/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from Messaging.views import HomeView
 from Messaging.views import ScheduleView
-from Messaging.views import Backend
+from Messaging.views import ScheduleMessageView
+from Messaging.views import BackendView
 from Messaging.views import AdminView
 
 urlpatterns = [
     path('', HomeView, name="home"),
     path('schedule/', ScheduleView),
-    path('backend/', Backend),
+    path('schedule-message/', ScheduleMessageView),
+    path('backend/', BackendView),
     path('adminpanel/', AdminView),
     path('admin/', admin.site.urls),
 ]
